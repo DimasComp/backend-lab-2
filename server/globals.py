2982e9e8a496af5ec1502aa93376280d75d2e7ea
DimasComp <d.demchyk.im21@kpi.ua>
2025-01-14 16:18:26 +0200

    Integrate jwt

--- server/globals.py
+++ server/globals.py
@@ -2,6 +2,9 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 import os
+from flask_restful import Api
+from flask_jwt_extended import JWTManager
+import server.jwt_setup
 
 os.environ.setdefault('POSTGRES_USER', 'mock_user')
 os.environ.setdefault('POSTGRES_PASSWORD', 'mock_password')
@@ -10,5 +13,9 @@ os.environ.setdefault('POSTGRES_DB', 'mock_db')
 
 app = Flask(__name__)
 app.config.from_pyfile('config.py', silent=True)
+app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_Key', 'mock_secret_key')
+
 db = SQLAlchemy(app)
+api = Api(app)
+jwt = JWTManager(app)
 migrate = Migrate(app, db)
